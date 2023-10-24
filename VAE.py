5e762ab9f4e0011e40ff6b2d8727939934a6c634
Torbidos7 <79851688+Torbidos7@users.noreply.github.com>
2023-10-24 16:52:42 +0200

    Corrected VAE

--- VAE.py
+++ VAE.py
@@ -1,231 +1,234 @@
-#https://youtu.be/YV9D3TWY5Zo
-#https://youtu.be/8wrLjnQ7EWQ
-
-"""
-VAEs can be used for generative purposes. 
-
-This code demonstrates VAE using MNIST dataset.
-Just like regular autoencoder VAE returns an array (image) of same domensions
-as input but we can introduce variation by tweaking the latent vector.
-
-"""
-
 import keras
+import tensorflow as tf 
 from keras.layers import Conv2D, Conv2DTranspose, Input, Flatten, Dense, Lambda, Reshape
-#from keras.layers import BatchNormalization
 from keras.models import Model
-from keras.datasets import mnist
-from keras import backend as K
 import numpy as np
 import matplotlib.pyplot as plt
+from keras import backend as K
 
-# Load MNIST
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+class VAE(tf.keras.Model):
+    """
+    Variational Autoencoder (VAE) model for image generation.
 
-#Normalize and reshape ============
+    Args:
+        image_shape (tuple): Shape of the input image.
+        depths (list): List of integers representing the number of filters in each convolutional layer.
+        initial_dim (int): Dimension of the first fully connected layer.
+        latent_dim (int): Dimension of the latent space.
 
-#Norm.
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train = x_train / 255
-x_test = x_test / 255
+    Attributes:
+        encoder (keras.Model): Encoder part of the VAE model.
+        decoder (keras.Model): Decoder part of the VAE model.
+        mse (keras.metrics.MeanSquaredError): Mean squared error metric.
+        mae (keras.metrics.MeanAbsoluteError): Mean absolute error metric.
+        kld (keras.metrics.KLDivergence): KL divergence metric.
 
-# Reshape 
-img_width  = x_train.shape[1]
-img_height = x_train.shape[2]
-num_channels = 1 #MNIST --> grey scale so 1 channel
-x_train = x_train.reshape(x_train.shape[0], img_height, img_width, num_channels)
-x_test = x_test.reshape(x_test.shape[0], img_height, img_width, num_channels)
-input_shape = (img_height, img_width, num_channels)
-# ========================
-#View a few images
-plt.figure(1)
-plt.subplot(221)
-plt.imshow(x_train[42][:,:,0])
+    Methods:
+        build_encoder(): Builds the encoder part of the VAE model.
+        build_decoder(): Builds the decoder part of the VAE model.
+        sampling(args): Samples from the latent space.
+        call(inputs): Calls the VAE model.
+        reconstruction_loss(inputs, reconstructed, z_mean, z_log_var): Calculates the reconstruction loss.
+        generate_batch(images, class_labels): Generates a batch of images.
+        generate_single(image, class_label): Generates a single image.
+        generate_embedding(image, class_label): Generates an embedding for an image.
+        train_step(data): Performs a training step.
+        test_step(data): Performs a testing step.
+        summary(): Prints a summary of the encoder and decoder models.
+    """
 
-plt.subplot(222)
-plt.imshow(x_train[420][:,:,0])
+    def __init__(self, image_shape, depths, initial_dim, latent_dim, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.image_shape = image_shape
+        self.depths = depths
+        self.initial_dim = initial_dim
+        self.latent_dim = latent_dim
 
-plt.subplot(223)
-plt.imshow(x_train[4200][:,:,0])
+        self.encoder = self.build_encoder()
+        self.decoder = self.build_decoder()
 
-plt.subplot(224)
-plt.imshow(x_train[42000][:,:,0])
-plt.show()
+        self.mse = tf.keras.metrics.MeanSquaredError(name='mse')
+        self.mae = tf.keras.metrics.MeanAbsoluteError(name='mae')
+        self.kld = tf.keras.metrics.KLDivergence(name='kld')
 
+    def build_encoder(self):
+        """
+        Builds the encoder part of the VAE model.
 
-# BUILD THE MODEL
+        Returns:
+            keras.Model: Encoder part of the VAE model.
+        """
+        inputs = Input(shape=self.image_shape)
+        x = inputs
+        for depth in self.depths:
+            x = Conv2D(depth, 3, strides=2, padding='same', activation='relu')(x)
+            x = Conv2D(depth, 3, strides=1, padding='same', activation='relu')(x)
+        x = Flatten()(x)
+        x = Dense(self.initial_dim, activation='relu')(x)
+        z_mean = Dense(self.latent_dim)(x)
+        z_log_var = Dense(self.latent_dim)(x)
+        z = Lambda(self.sampling)([z_mean, z_log_var])
+        encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+        return encoder
 
-# # ================= #############
-# # Encoder
-#Let us define 4 conv2D, flatten and then dense
-# # ================= ############
+    def build_decoder(self):
+        """
+        Builds the decoder part of the VAE model.
 
-latent_dim = 2 # Number of latent dim parameters
+        Returns:
+            keras.Model: Decoder part of the VAE model.
+        """
+        latent_inputs = Input(shape=(self.latent_dim,))
+        x = Dense(self.initial_dim, activation='relu')(latent_inputs)
+        x = Dense(self.depths[-1] * self.image_shape[0] // (2 ** len(self.depths)) * self.image_shape[1] // (2 ** len(self.depths)), activation='relu')(x)
+        x = Reshape((self.image_shape[0] // (2 ** len(self.depths)), self.image_shape[1] // (2 ** len(self.depths)), self.depths[-1]))(x)
+        for depth in reversed(self.depths[:-1]):
+            x = Conv2DTranspose(depth, 3, strides=2, padding='same', activation='relu')(x)
+            x = Conv2DTranspose(depth, 3, strides=1, padding='same', activation='relu')(x)
+        x = Conv2DTranspose(self.image_shape[-1], 3, strides=2, padding='same', activation='sigmoid')(x)
+        decoder = Model(latent_inputs, x, name='decoder')
+        return decoder
 
-input_img = Input(shape=input_shape, name='encoder_input')
-x = Conv2D(32, 3, padding='same', activation='relu')(input_img)
-x = Conv2D(64, 3, padding='same', activation='relu',strides=(2, 2))(x)
-x = Conv2D(64, 3, padding='same', activation='relu')(x)
-x = Conv2D(64, 3, padding='same', activation='relu')(x)
+    def sampling(self, args):
+        """
+        Samples from the latent space.
 
-conv_shape = K.int_shape(x) #Shape of conv to be provided to decoder
-#Flatten
-x = Flatten()(x)
-x = Dense(32, activation='relu')(x)
+        Args:
+            args (list): List of tensors representing the mean and log variance of the latent space.
 
-# Two outputs, for latent mean and log variance (std. dev.)
-#Use these to sample random variables in latent space to which inputs are mapped. 
-z_mu = Dense(latent_dim, name='latent_mu')(x)   #Mean values of encoded input
-z_sigma = Dense(latent_dim, name='latent_sigma')(x)  #Std dev. (variance) of encoded input
+        Returns:
+            tensor: Sampled tensor from the latent space.
+        """
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], self.latent_dim), mean=0., stddev=1.)
+        return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
-#REPARAMETERIZATION TRICK
-# Define sampling function to sample from the distribution
-# Reparameterize sample based on the process defined by Gunderson and Huang
-# into the shape of: mu + sigma squared x eps
-#This is to allow gradient descent to allow for gradient estimation accurately. 
-def sample_z(args):
-  z_mu, z_sigma = args
-  eps = K.random_normal(shape=(K.shape(z_mu)[0], K.int_shape(z_mu)[1]))
-  return z_mu + K.exp(z_sigma / 2) * eps
-
-# sample vector from the latent distribution
-# z is the labda custom layer we are adding for gradient descent calculations
-  # using mu and variance (sigma)
-z = Lambda(sample_z, output_shape=(latent_dim, ), name='z')([z_mu, z_sigma])
-
-#Z (lambda layer) will be the last layer in the encoder.
-# Define and summarize encoder model.
-encoder = Model(input_img, [z_mu, z_sigma, z], name='encoder')
-print(encoder.summary())
-
-# ================= ###########
-# Decoder
-#
-# ================= #################
-
-# decoder takes the latent vector as input
-decoder_input = Input(shape=(latent_dim, ), name='decoder_input')
-
-# Need to start with a shape that can be remapped to original image shape as
-#we want our final utput to be same shape original input.
-#So, add dense layer with dimensions that can be reshaped to desired output shape
-x = Dense(conv_shape[1]*conv_shape[2]*conv_shape[3], activation='relu')(decoder_input)
-# reshape to the shape of last conv. layer in the encoder, so we can 
-x = Reshape((conv_shape[1], conv_shape[2], conv_shape[3]))(x)
-# upscale (conv2D transpose) back to original shape
-# use Conv2DTranspose to reverse the conv layers defined in the encoder
-x = Conv2DTranspose(32, 3, padding='same', activation='relu',strides=(2, 2))(x)
-#Can add more conv2DTranspose layers, if desired. 
-#Using sigmoid activation
-x = Conv2DTranspose(num_channels, 3, padding='same', activation='sigmoid', name='decoder_output')(x)
-
-# Define and summarize decoder model
-decoder = Model(decoder_input, x, name='decoder')
-decoder.summary()
-
-# apply the decoder to the latent sample 
-z_decoded = decoder(z)
-
-
-# =========================
-#Define custom loss
-#VAE is trained using two loss functions reconstruction loss and KL divergence
-#Let us add a class to define a custom layer with loss
-class CustomLayer(keras.layers.Layer):
-
-    def vae_loss(self, x, z_decoded):
-        x = K.flatten(x)
-        z_decoded = K.flatten(z_decoded)
-        
-        # Reconstruction loss (as we used sigmoid activation we can use binarycrossentropy)
-        recon_loss = keras.metrics.binary_crossentropy(x, z_decoded)
-        
-        # KL divergence
-        kl_loss = -5e-4 * K.mean(1 + z_sigma - K.square(z_mu) - K.exp(z_sigma), axis=-1)
-        return K.mean(recon_loss + kl_loss)
-
-    # add custom loss to the class
     def call(self, inputs):
-        x = inputs[0]
-        z_decoded = inputs[1]
-        loss = self.vae_loss(x, z_decoded)
-        self.add_loss(loss, inputs=inputs)
-        return x
+        """
+        Calls the VAE model.
 
-# apply the custom loss to the input images and the decoded latent distribution sample
-y = CustomLayer()([input_img, z_decoded])
-# y is basically the original image after encoding input img to mu, sigma, z
-# and decoding sampled z values.
-#This will be used as output for vae
+        Args:
+            inputs (tensor): Input tensor.
 
-# =================
-# VAE 
-# =================
-vae = Model(input_img, y, name='vae')
+        Returns:
+            tensor: Reconstructed tensor.
+        """
+        z_mean, z_log_var, z = self.encoder(inputs)
+        reconstructed = self.decoder(z)
+        self.add_loss(self.reconstruction_loss(inputs, reconstructed, z_mean, z_log_var), name='loss')
+        self.add_metric(self.mse(inputs, reconstructed), name='mse')
+        self.add_metric(self.mae(inputs, reconstructed), name='mae')
+        self.add_metric(self.kld(z_mean, z_log_var), name='kld')
+        return reconstructed
 
-# Compile VAE
-vae.compile(optimizer='adam', loss=None)
-vae.summary()
+    def reconstruction_loss(self, inputs, reconstructed, z_mean, z_log_var):
+        """
+        Calculates the reconstruction loss.
 
-# Train autoencoder
-vae.fit(x_train, None, epochs = 10, batch_size = 32, validation_split = 0.2)
+        Args:
+            inputs (tensor): Input tensor.
+            reconstructed (tensor): Reconstructed tensor.
+            z_mean (tensor): Mean tensor of the latent space.
+            z_log_var (tensor): Log variance tensor of the latent space.
 
-# =================
-# Visualize results
-# =================
-#Visualize inputs mapped to the Latent space
-#Remember that we have encoded inputs to latent space dimension = 2. 
-#Extract z_mu --> first parameter in the result of encoder prediction representing mean
+        Returns:
+            tensor: Reconstruction loss tensor.
+        """
+        reconstruction_loss = self.mse(inputs, reconstructed)
+        kl_loss = -0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+        return K.mean(reconstruction_loss + kl_loss)
 
-mu, _, _ = encoder.predict(x_test)
-#Plot dim1 and dim2 for mu
-plt.figure(figsize=(10, 10))
-plt.scatter(mu[:, 0], mu[:, 1], c=y_test, cmap='brg')
-plt.xlabel('dim 1')
-plt.ylabel('dim 2')
-plt.colorbar()
-plt.show()
+    def generate_batch(self, images, class_labels):
+        """
+        Generates a batch of images.
 
+        Args:
+            images (tensor): Input tensor.
+            class_labels (tensor): Class label tensor.
 
-# Visualize images
-#Single decoded image with random input latent vector (of size 1x2)
-#Latent space range is about -5 to 5 so pick random values within this range
-#Try starting with -1, 1 and slowly go up to -1.5,1.5 and see how it morphs from 
-#one image to the other.
-sample_vector = np.array([[1,-1]])
-decoded_example = decoder.predict(sample_vector)
-decoded_example_reshaped = decoded_example.reshape(img_width, img_height)
-plt.imshow(decoded_example_reshaped)
+        Returns:
+            tensor: Generated tensor.
+        """
+        z_mean, _, _ = self.encoder(images)
+        generated = self.decoder(z_mean)
+        return generated
 
-#Let us automate this process by generating multiple images and plotting
-#Use decoder to generate images by tweaking latent variables from the latent space
-#Create a grid of defined size with zeros. 
-#Take sample from some defined linear space. In this example range [-4, 4]
-#Feed it to the decoder and update zeros in the figure with output.
+    def generate_single(self, image, class_label):
+        """
+        Generates a single image.
 
+        Args:
+            image (tensor): Input tensor.
+            class_label (tensor): Class label tensor.
 
-n = 20  # generate 15x15 digits
-figure = np.zeros((img_width * n, img_height * n, num_channels))
+        Returns:
+            tensor: Generated tensor.
+        """
+        images = tf.expand_dims(image, axis=0)
+        z_mean, _, _ = self.encoder(images)
+        generated = self.decoder(z_mean)
+        return generated
 
-#Create a Grid of latent variables, to be provided as inputs to decoder.predict
-#Creating vectors within range -5 to 5 as that seems to be the range in latent space
-grid_x = np.linspace(-5, 5, n)
-grid_y = np.linspace(-5, 5, n)[::-1]
+    def generate_embedding(self, image, class_label):
+        """
+        Generates an embedding for an image.
 
-# decoder for each square in the grid
-for i, yi in enumerate(grid_y):
-    for j, xi in enumerate(grid_x):
-        z_sample = np.array([[xi, yi]])
-        x_decoded = decoder.predict(z_sample)
-        digit = x_decoded[0].reshape(img_width, img_height, num_channels)
-        figure[i * img_width: (i + 1) * img_width,
-               j * img_height: (j + 1) * img_height] = digit
+        Args:
+            image (tensor): Input tensor.
+            class_label (tensor): Class label tensor.
 
-plt.figure(figsize=(10, 10))
-#Reshape for visualization
-fig_shape = np.shape(figure)
-figure = figure.reshape((fig_shape[0], fig_shape[1]))
+        Returns:
+            tensor: Embedding tensor.
+        """
+        images = tf.expand_dims(image, axis=0)
+        _, _, z = self.encoder(images)
+        return z
 
-plt.imshow(figure, cmap='gnuplot2')
-plt.show()  
+    def train_step(self, data):
+        """
+        Performs a training step.
+
+        Args:
+            data (tuple): Tuple of input tensor, target tensor, and class label tensor.
+
+        Returns:
+            dict: Dictionary of metric names and results.
+        """
+        images, _, labels = data
+
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(images)
+            reconstructed = self.decoder(z)
+            loss = self.reconstruction_loss(images, reconstructed, z_mean, z_log_var)
+
+        gradients = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_weights))
+
+        self.compiled_metrics.update_state(labels, reconstructed)
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, data):
+        """
+        Performs a testing step.
+
+        Args:
+            data (tuple): Tuple of input tensor, target tensor, and class label tensor.
+
+        Returns:
+            dict: Dictionary of metric names and results.
+        """
+        images, _, labels = data
+
+        z_mean, z_log_var, z = self.encoder(images)
+        reconstructed = self.decoder(z)
+        loss = self.reconstruction_loss(images, reconstructed, z_mean, z_log_var)
+
+        self.compiled_metrics.update_state(labels, reconstructed)
+        return {m.name: m.result() for m in self.metrics}
+
+    def summary(self):
+        """
+        Prints a summary of the encoder and decoder models.
+        """
+        self.encoder.summary()
+        self.decoder.summary()
