724e4c14c5962f2dc8e687dc76eb0bb745831566
Torbidos7 <danybuschi@gmail.com>
2023-10-25 15:32:13 +0200

    added residual blocks

--- VAE.py
+++ VAE.py
@@ -5,7 +5,69 @@ from tensorflow.keras.models import Model
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import backend as K
+from tensorflow.keras import layers
 
+class ResidualBlock(layers.Layer):
+    def __init__(self, output_dim, activation="relu", **kwargs) -> None:
+        """Standard residual block with pre-activation https://github.com/GianlucaCarlini/latent_diffusion_tf/blob/main/source/blocks.py
+
+        Args:
+            output_dim (int): The output channel dimension.
+            norm_layer (tf.keras.layers.Layer, optional): The normalization layer
+                to be applied befor the activation and the convolution. Defaults to None.
+                If None, LayerNormalization is applied.
+            activation (str, optional): The activation function for the residual.
+                Defaults to "swish".
+        """
+        super().__init__(**kwargs)
+
+        self.outpud_dim = output_dim
+
+        self.norm1 = layers.LayerNormalization(epsilon=1e-5)
+        self.norm2 = layers.LayerNormalization(epsilon=1e-5)
+
+        self.act1 = tf.keras.activations.get(activation)
+        self.conv1 = layers.Conv2D(output_dim, kernel_size=3, padding="same")
+        self.act2 = layers.Activation(activation=activation)
+        self.conv2 = layers.Conv2D(output_dim, kernel_size=3, padding="same")
+        self.proj = layers.Conv2D(output_dim, kernel_size=1)
+
+    def call(self, inputs):
+        '''
+        This is where the layer's logic lives.
+        
+        Args:
+            inputs (tensor): Input tensor.
+            
+        Returns:
+            tensor: Output tensor.
+        '''
+
+        x = self.norm1(inputs)
+        x = self.act1(x)
+        x = self.conv1(x)
+
+        x = self.norm2(x)
+        x = self.act2(x)
+        x = self.conv2(x)
+
+        if self.outpud_dim != inputs.shape[-1]:
+            inputs = self.proj(inputs)
+
+        return x + inputs
+
+    def get_config(self):
+        '''
+        Returns the config of the layer. This is used for saving and loading from a model
+        '''
+        config = super().get_config()
+        config.update(
+            {
+                "output_dim": self.outpud_dim,
+            }
+        )
+        return config
+    
 class VAE(tf.keras.Model):
     """
     Variational Autoencoder (VAE) model for image generation.
@@ -13,6 +75,7 @@ class VAE(tf.keras.Model):
     Args:
         image_shape (tuple): Shape of the input image.
         depths (list): List of integers representing the number of filters in each convolutional layer.
+        initial_dim (int): Dimension of the residual blocks.
         dense_dim (int): Dimension of the first fully connected layer.
         latent_dim (int): Dimension of the latent space.co
 
@@ -37,12 +100,15 @@ class VAE(tf.keras.Model):
         summary(): Prints a summary of the encoder and decoder models.
     """
 
-    def __init__(self, image_shape, depths, dense_dim, latent_dim, **kwargs):
+    def __init__(self, image_shape, depths, initial_dim, dense_dim, latent_dim, **kwargs):
         super(VAE, self).__init__(**kwargs)
         self.image_shape = image_shape
         self.depths = depths
+        self.initial_dim = initial_dim
         self.dense_dim = dense_dim
         self.latent_dim = latent_dim
+        self.embed_dims = [initial_dim * 2**i for i in range(len(depths))]
+
 
         self.encoder = self.build_encoder()
         self.decoder = self.build_decoder()
@@ -60,9 +126,14 @@ class VAE(tf.keras.Model):
         """
         inputs = Input(shape=self.image_shape)
         x = inputs
-        for depth in self.depths:
-            x = Conv2D(depth, 3, strides=2, padding='same', activation='relu')(x)
-            x = Conv2D(depth, 3, strides=1, padding='same', activation='relu')(x)
+        
+        for i, depth in enumerate(self.depths):
+            for _ in range(depth):
+
+                x = ResidualBlock(output_dim=self.embed_dims[i], activation="swish")(x)
+
+            if i < len(self.depths) - 1:
+                x = tf.keras.layers.Conv2D(filters=self.embed_dims[i], kernel_size=2, strides=2, padding="same")(x)
         x = Flatten()(x)
         x = Dense(self.dense_dim, activation='relu')(x)
         z_mean = Dense(self.latent_dim)(x)
@@ -84,10 +155,11 @@ class VAE(tf.keras.Model):
         # dimensions must be reshapable to the initial image dimensions
         x = Dense(self.depths[-1] * self.image_shape[0] // (2 ** len(self.depths)) * self.image_shape[1] // (2 ** len(self.depths)), activation='relu')(x)
         x = Reshape((self.image_shape[0] // (2 ** len(self.depths)), self.image_shape[1] // (2 ** len(self.depths)), self.depths[-1]))(x)
-        for depth in reversed(self.depths[:-1]):
-            x = Conv2DTranspose(depth, 3, strides=2, padding='same', activation='relu')(x)
-            x = Conv2DTranspose(depth, 3, strides=1, padding='same', activation='relu')(x)
-        x = Conv2DTranspose(self.image_shape[-1], 3, strides=2, padding='same', activation='sigmoid')(x)
+        for i, depth in enumerate(self.depths[1:]):
+            x = tf.keras.layers.UpSampling2D(size=(2, 2), interpolation="bicubic")(x)
+            for _ in range(depth):
+                x = ResidualBlock(output_dim=self.embed_dims[i + 1], activation="swish")(x)
+        x = Conv2DTranspose(filters=self.image_shape[-1], kernel_size=3, strides=2, padding='same', activation='sigmoid')(x)
         decoder = Model(latent_inputs, x, name='decoder')
         return decoder
 
@@ -234,3 +306,13 @@ class VAE(tf.keras.Model):
         """
         self.encoder.summary()
         self.decoder.summary()
+        encoder_trainable_count = np.sum([K.count_params(w) for w in self.encoder.trainable_weights], dtype=np.int32)
+        decoder_trainable_count = np.sum([K.count_params(w) for w in self.decoder.trainable_weights], dtype=np.int32)
+        print('#' * 80)
+        print('VAE Summary' + '\n')
+        print("Total Parameters:", "{:,}".format(self.encoder.count_params() + self.decoder.count_params()))
+        print('Trainable Parameters:', "{:,}".format(encoder_trainable_count + decoder_trainable_count))
+        print('Non-Trainable Parameters:', "{:,}".format(self.encoder.count_params() + self.decoder.count_params() - encoder_trainable_count - decoder_trainable_count))        
+        print('#' * 80)
+       
+        
